4ded25c6a48a81a57f0515ca001d4a5324da18d0
tao <tao@qq.com>
2018-11-05 14:06:53 +0800

    't'

--- RedisClass.php
+++ RedisClass.php
@@ -10,14 +10,10 @@ class RedisClass
 {
     private static $_instance = [];
 
-    private static $dbNum = 0;
-
-    private $redisException = '';
-
-    private $redis = [];
+    private static $redis = [];
 
 
-    private function __construct($host = '127.0.0.1', $port = '6379', $timeout = '0', $password = '')
+    private function __construct($dbNum, $host = '127.0.0.1', $port = '6379', $timeout = '0', $password = '')
     {
         try {
             if (!extension_loaded('redis')) {
@@ -27,11 +23,11 @@ class RedisClass
             exit($e -> getMessage());
         }
 
-        $this->redis[self::$dbNum] = new Redis();
+        self::$redis[$dbNum] = new Redis();
         try {
-            $this->redis[self::$dbNum]->pconnect($host, $port);
+            self::$redis[$dbNum]->pconnect($host, $port);
             if ($password != '') {
-                $this->redis[self::$dbNum]->auth($password);
+                self::$redis[$dbNum]->auth($password);
             }
         } catch (Exception $e) {
             echo '错误代码：'.$e -> getCode().'错误信息'.$e -> getMessage();
@@ -41,30 +37,25 @@ class RedisClass
     private function __clone(){}
 
 
-    public static function getSingleInstance($host, $port, $timeout = 2, $password = '')
+    public static function getSingleInstance($dbNum = 0, $host, $port, $timeout = 2, $password = '')
     {
-        self::$dbNum++;
         try {
-            if (isset(self::$_instance[self::$dbNum]) && self::$_instance[self::$dbNum]->redis[self::$dbNum]->Ping() == '+PONG') {
-                return self::$_instance[self::$dbNum];
+            if (isset(self::$_instance[$dbNum]) && self::$_instance[$dbNum]->redis[$dbNum]->Ping() == '+PONG') {
+                return self::$_instance[$dbNum];
             }
         } catch (Exception $e) {
 
         }
-        self::$_instance[self::$dbNum] = new self($host, $port, $timeout, $password);
-        return self::$_instance[self::$dbNum];
+        self::$_instance[$dbNum] = new self($host, $port, $timeout, $password);
+        return self::$_instance[$dbNum];
+    }
+
+
+    protected static function set($dbNum, $key, $val)
+    {
+        var_dump(self::$redis[$dbNum] -> ping());
     }
 }
 
-$redis = RedisClass::getSingleInstance('127.0.0.1', '6379');
-echo '<pre>';
-var_dump($redis);
-
-$redis = new Redis();
-
-try {
-    $redis -> connect('127.0.0.1', '6379');
-}catch(Exception $e){
-    echo $e -> getMessage();
-}
-var_dump($redis -> ping());
+$redis = RedisClass::getSingleInstance(0, '127.0.0.1', '6379');
+$redis::set(0, 'a', 'c');

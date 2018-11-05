9f92b628e20b545e0948af5db7e4365aa9a6e2cb
tao <tao@qq.com>
2018-11-05 14:58:46 +0800

    't'

--- RedisClass.php
+++ RedisClass.php
@@ -53,6 +53,67 @@ class RedisClass
         return self::$_instance[$dbNum];
     }
 
+    ################### 通用操作 ################
+
+    public function delete($dbNum, $key)
+    {
+        if (!self::$redis[$dbNum]->delete($key)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 设置时间段
+     * @param $dbNum
+     * @param $time int 过期时间段
+     */
+    public function expire($dbNum, $key, $time)
+    {
+        if (self::$redis[$dbNum]->expire($key, $time)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 设置过期时间戳
+     * @param $dbNum
+     * @param $key
+     * @param $timestamp
+     * @return bool
+     */
+    public function expireAt($dbNum, $key, $timestamp)
+    {
+        if (self::$redis[$dbNum]->expireAt($key, $timestamp)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param $dbNum
+     * @param $key
+     * @param bool $isP  是否获取毫秒
+     * @return bool
+     */
+    public function getExpireTime($dbNum, $key, $isP = false)
+    {
+        if((bool)$isP){
+            $time = self::$redis[$dbNum] -> ttl($key);
+        } else {
+            $time = self::$redis[$dbNum] -> pttl($key);
+        }
+
+        if($time){
+            return $time;
+        }else{
+            return false;
+        }
+    }
 
     public function set($dbNum, $key, $val)
     {
@@ -69,13 +130,25 @@ class RedisClass
 
     public function get($dbNum, $key)
     {
-        if(! self::$redis[$dbNum] -> exists($key)){
+        $string = self::$redis[$dbNum]->get($key);
+        if ($string) {
+
+            return self::$redis[$dbNum]->get($key);
+
+        } else {
+
             return false;
+
         }
 
-        return self::$redis[$dbNum] -> get($key);
     }
 }
 
 $redis = RedisClass::getSingleInstance(0, '127.0.0.1', '6379');
-echo $redis -> get(0, 'a');
+echo $redis->get(0, 'a');
+$redis -> expire(0, 'a', 3600);
+echo '时间段'.$redis -> getExpireTime(0, 'a').'<br>';
+$redis -> expire(0, 'a', strtotime('+1day'));
+echo '时间戳'.$redis -> getExpireTime(0, 'a').'<br>';
+
+echo '毫秒'.$redis -> getExpireTime(0, 'a');
